8cbc5f2eb0845c586d1a8fe555cdc48f5f8ba836
ChairukXa <69110817+ChairukXa@users.noreply.github.com>
2022-04-24 21:25:32 +0700

    update Navbar

--- src/component/AppNavbar.js
+++ src/component/AppNavbar.js
@@ -1,19 +1,26 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React,{useState} from 'react'
+import { Link } from 'react-router-dom'
 import "./AppNavbar.css"
+import { FaBars } from "react-icons/fa"
+import { ImCross } from "react-icons/im"
 
 const AppNavbar = () => {
+  const [Moblie,setMoblie] = useState(false)
   return (
     <nav className='Navbar'>
       <div className='contrainer'> 
         <h3 className='logo'>YGO Community</h3> 
-        <ul className='Nav-Links'>
+        <ul className={Moblie? "Nav-Links-moblie":'Nav-Links'} onClick={()=>setMoblie(false)}>
           <Link to="/"><li>Home</li></Link>
           <Link to="/rulebook"><li>RuleBook</li></Link>
           <Link to="/archetype"><li>Archetype</li></Link>
           <Link to="/search"><li>Search</li></Link>
           <Link to="/account"><li>Sign-In</li></Link>
         </ul>
+        <button className='Moblie_menu_icon' onClick={()=>setMoblie(!Moblie)}>
+          {Moblie? < ImCross/>: <FaBars/> }
+          <FaBars />
+        </button>
       </div>
     </nav>
   );
